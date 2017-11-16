a7825332019651ec6dbce8df57ba53c814f78847
LeeoCSL <leonardoribeirocsl@gmail.com>
2017-11-16 14:38:49 -0200

    logo

---
+++ app/src/main/java/br/com/consultai/Fragments/EditarFragment.java
@@ -0,0 +1,29 @@
+package br.com.consultai.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.consultai.R;
+
+
+public class EditarFragment extends Fragment {
+
+
+    public EditarFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_editar, container, false);
+    }
+
+
+}
