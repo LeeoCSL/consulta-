82c29b3a9d929de452ce78f98133cc75d03ccd58
Renan Rogério Boni <renan.boni@vbs.corp>
2017-11-23 12:38:17 -0200

    login

--- app/src/main/java/br/com/consultai/activities/LoginActivity.java
+++ app/src/main/java/br/com/consultai/activities/LoginActivity.java
@@ -115,6 +115,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
